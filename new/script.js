e8167168ae2663f98c8fd26a90a9340547562f50
Roman <rvtsukanov@edu.hse.ru>
2018-08-19 18:29:51 +0300

    First successes with meses

--- new/script.js
+++ new/script.js
@@ -1,33 +1,39 @@
 console.log(navigator.getGamepads())
-
 var gamepads = navigator.getGamepads()
 
-//console.log(gamepads[0].buttons.length)
+
+
+//console.log(gamepads[0].buttons.length
+//console.log(gamepads[0].buttons[0].pressed == false)
+
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+var renderer = new THREE.WebGLRenderer();
+
+renderer.setSize( window.innerWidth, window.innerHeight );
+
+
+console.log(document.body)
+document.body.appendChild( renderer.domElement );
+
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
 
 
 
-function a(){
-alert(1);
-}
+camera.position.z = 5;
 
 
-
-
-
-console.log(gamepads[0].buttons[0].pressed == false)
 
 var i = 0;
+
 function checker(){
-	var gamepad = navigator.getGamepads()[0];
+    requestAnimationFrame(checker);
 
-//	for(i = 0; i < gamepads[0].buttons.length; i++){
-//		if(gamepads[0].buttons[i].pressed == true){
-//			console.log(i, 'is pressed');
-//		}
-//	}
-
-	
-
+    /*
+    var gamepad = navigator.getGamepads()[0];
 	var back = document.getElementsByTagName('body');
 	pos = gamepad.axes[0];
 	console.log(pos);
@@ -36,10 +42,19 @@ function checker(){
 	document.body.style.backgroundColor = 'rgb(' + Math.ceil(a).toString() + ',' + 0 + ',' + 0 + ')';
 	console.log(document.body.style.backgroundColor);
 
+    */
 
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render( scene, camera );
 
 }
 
-setInterval(checker, 50);
+checker();
+
+//setInterval(checker, 50);
+
+//requestAnimationFrame(checker, 1000);
 
 
